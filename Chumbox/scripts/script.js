160faaa9920fa0ac3728c6016008ccd6586cedde
Alex Benzer <abenzer@gmail.com>
2019-11-23 17:00:43 -0800

    Update

--- Chumbox/scripts/script.js
+++ Chumbox/scripts/script.js
@@ -1,17 +1,18 @@
-// Load in the required modules
-const NativeUI = require('NativeUI');
-const Scene = require('Scene');
-const TouchGestures = require('TouchGestures');
-
-// Create a variable to store the name of our text object
-// var textNodeName  = 'captionCamera';
-
-makeEditable('captionCamera');
-makeEditable('box2Caption');
-
-function makeEditable(textNodeName) {
- var text = Scene.root.find(textNodeName);
- TouchGestures.onTap(text).subscribe(function() {
-   NativeUI.enterTextEditMode(textNodeName);
- });
-}
+// // Load in the required modules
+// const NativeUI = require('NativeUI');
+// const Scene = require('Scene');
+// const TouchGestures = require('TouchGestures');
+//
+// // Create a variable to store the name of our text object
+// // var textNodeName  = 'captionCamera';
+//
+// makeEditable('captionCamera');
+// makeEditable('box1Caption');
+// makeEditable('box2Caption');
+//
+// function makeEditable(textNodeName) {
+//  var text = Scene.root.find(textNodeName);
+//  TouchGestures.onTap(text).subscribe(function() {
+//    NativeUI.enterTextEditMode(textNodeName);
+//  });
+// }
