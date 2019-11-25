df0a9430342657c686f87ec7b9dafd64afeee378
Alex Benzer <abenzer@gmail.com>
2019-11-24 22:24:09 -0800

    Final

--- Chumbox2/scripts/script.js
+++ Chumbox2/scripts/script.js
@@ -5,10 +5,19 @@ const Scene = require('Scene');
 const Materials = require('Materials');
 const Textures = require('Textures');
 const TouchGestures = require('TouchGestures');
+const Instruction = require('Instruction');
+const Time = require('Time');
 var Patches = require('Patches');
 
-textures = [1, 2, 3, 4, 10, 6, 7, 8, 9, 11];
-setTextures();
+// Define a boolean that will be true until 5 seconds has passed
+// var show2 = Time.ms.lt(5000);
+// Instruction.bind(show2, 'tap_to_change');
+// var show1 = Time.ms.lt(5000);
+// Instruction.bind(show1, 'find_face');
+
+
+textures = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+setTextures();  
 
 var triggerIn = Patches.getPulseValue('triggerIn');
 triggerIn.subscribe( function(e) {
@@ -20,7 +29,7 @@ function setTextures() {
   shuffle(textures);
   for(i=0;i<10;i++) {
     if(i != 5) {
-      Diagnostics.log(i + ' | ' + textureIndex);
+      // Diagnostics.log(i + ' | ' + textureIndex);
       var material = Materials.get('material' + i);
       var textureIndex = textures[i];
       var texture = Textures.get('Slice ' + textureIndex);
