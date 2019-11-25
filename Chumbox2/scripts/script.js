f62fe92a0248f91dd60508c3d020da280e562e93
Alex Benzer <abenzer@gmail.com>
2019-11-24 16:12:27 -0800

    Textures

--- Chumbox2/scripts/script.js
+++ Chumbox2/scripts/script.js
@@ -5,22 +5,22 @@ const Scene = require('Scene');
 const Materials = require('Materials');
 const Textures = require('Textures');
 const TouchGestures = require('TouchGestures');
+var Patches = require('Patches');
 
-textures = [1, 2, 3, 4, 4, 6, 7, 8, 9];
+textures = [1, 2, 3, 4, 4, 6, 7, 8, 9, 4];
 setTextures();
 
-
-const screen = Scene.root.find('Camera');
-TouchGestures.onTap(screen).subscribe(function (gesture) {
-  Diagnostics.log('reset');
+var triggerIn = Patches.getPulseValue('triggerIn');
+triggerIn.subscribe( function(e) {
   setTextures();
 });
 
 
 function setTextures() {
   shuffle(textures);
-  for(i=1;i<9;i++) {
+  for(i=0;i<10;i++) {
     if(i != 5) {
+      Diagnostics.log(i + ' | ' + textureIndex);
       var material = Materials.get('material' + i);
       var textureIndex = textures[i];
       var texture = Textures.get('Slice ' + textureIndex);
